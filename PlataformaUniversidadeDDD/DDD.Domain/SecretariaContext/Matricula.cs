9c348a99911d3e14bbf8421cc8a55e22d5c6cce6
navarrounimar <navarrounimar@gmail.com>
2023-10-02 20:24:14 -0300

    correçã, tirando AlunoId de Matricula, já que não temos mais AlunoId e sim UserId. Mesmo assim o relacionamento se dá de forma correta.

--- PlataformaUniversidadeDDD/DDD.Domain/SecretariaContext/Matricula.cs
+++ PlataformaUniversidadeDDD/DDD.Domain/SecretariaContext/Matricula.cs
@@ -11,10 +11,8 @@ namespace DDD.Domain.SecretariaContext
     {
         public int MatriculaId { get; set; }
 
-        public int AlunoId { get; set; }
         public Aluno Aluno { get; set; }
 
-        public int DisciplinaId { get; set; }
         public Disciplina Disciplina { get; set; }
 
         public DateTime Data { get; set; }
